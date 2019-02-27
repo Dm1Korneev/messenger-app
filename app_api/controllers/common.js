ec2a6701812df323e7cc6809ebc3271924e74228
St1llman <dm1korneev@gmail.com>
2019-02-27 13:57:59 +0300

    add user info in message list output

--- app_api/controllers/common.js
+++ app_api/controllers/common.js
@@ -1,4 +1,10 @@
+var jwt = require("jsonwebtoken");
+
 module.exports.sendJsResponse = function(res, status, content) {
   res.status(status);
   res.json(content);
+};
+
+module.exports.parseToken = function(authorizationHeader) {
+  return jwt.decode(authorizationHeader.split(" ")[1]);
 };
--- app_api/controllers/messages.js
+++ app_api/controllers/messages.js
@@ -1,22 +1,54 @@
 var mongoose = require("mongoose");
 var MessageModel = mongoose.model("Message");
-var { sendJsResponse } = require("./common");
+var UserModel = mongoose.model("User");
+var { sendJsResponse, parseToken } = require("./common");
 
 module.exports.getMessages = function(req, res, next) {
-  MessageModel.find().exec(function(err, messages) {
-    if (err) {
-      sendJsResponse(res, 400, err);
-      return;
+  MessageModel.aggregate(
+    [
+      {
+        $lookup: {
+          from: "users",
+          localField: "author",
+          foreignField: "_id",
+          as: "fromItems"
+        }
+      },
+      {
+        $replaceRoot: {
+          newRoot: {
+            $mergeObjects: [{ $arrayElemAt: ["$fromItems", 0] }, "$$ROOT"]
+          }
+        }
+      },
+      {
+        $project: {
+          _id: 1,
+          avatar: 1,
+          dateTime: 1,
+          text: 1,
+          authorId: "$author",
+          author: "$name"
+        }
+      }
+    ],
+    function(err, messages) {
+      if (err) {
+        sendJsResponse(res, 400, err);
+        return;
+      }
+      sendJsResponse(res, 200, { messages });
     }
-    sendJsResponse(res, 200, { messages: messages });
-  });
+  );
 };
 
 module.exports.postMessage = function(req, res, next) {
+  const userInfo = parseToken(req.headers.authorization);
+
   MessageModel.create(
     {
       text: req.body.message.text,
-      author: "!!! TEST !!!"
+      author: userInfo._id
     },
     function(err, message) {
       if (err) {
