6c29b7f1c14e042a86737d410a738eb313f8b459
Dmitriy Korneev <dm1korneev@gmail.com>
2019-03-03 16:48:52 +0300

    add chats, users, messages routes to API

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 var jwt = require("express-jwt");
 var ctrlMessages = require("../controllers/messages");
+var ctrlChats = require("../controllers/chats");
+var ctrlUsers = require("../controllers/users");
 var ctrlAuth = require("../controllers/authentifications");
 
 const auth = jwt({
@@ -9,8 +11,18 @@ const auth = jwt({
   userProperty: "payload"
 });
 
-router.get("", auth, ctrlMessages.getMessages);
-router.post("", auth, ctrlMessages.postMessage);
+// chats
+router.get("/chats", auth, ctrlChats.getChats);
+router.get("/chats/:chatId", auth, ctrlChats.getChatByID);
+router.post("/chats", auth, ctrlChats.postChat);
+
+// messages
+router.get("/chats/:chatId/messages", auth, ctrlMessages.getMessages);
+router.post("/chats/:chatId/messages", auth, ctrlMessages.postMessage);
+
+// users
+router.get("/users/:userId", auth, ctrlUsers.getUserByID);
+router.get("/users", auth, ctrlUsers.getUsers);
 
 // auth
 router.post("/register", ctrlAuth.register);
