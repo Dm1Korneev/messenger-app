6886bf19994b18bc9036c17494de38fe36fc0fe7
Dmitriy Korneev <dm1korneev@gmail.com>
2019-03-03 20:36:34 +0300

    add admin field in chat schema

--- app_api/models/chats.js
+++ app_api/models/chats.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const chatSchema = new mongoose.Schema({
   title: { type: String, required: true },
+  admin: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }]
 });
 
