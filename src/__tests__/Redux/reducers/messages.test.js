71938bbd7806f41fd9021f796b5bd3c2ebb57b6d
Dmitriy Korneev <Dm1korneev@gmail.com>
2019-08-29 12:46:07 +0300

    add test to messages reducer

--- src/__tests__/Redux/reducers/messages.test.js
+++ src/__tests__/Redux/reducers/messages.test.js
@@ -8,7 +8,7 @@ describe('chats reducer', () => {
     );
   });
 
-  test('should handle ADD_USERS', () => {
+  test('should handle ADD_MESSAGES with array payload', () => {
     expect(
       reducer({}, {
         type: actionNames.ADD_MESSAGES,
@@ -28,9 +28,56 @@ describe('chats reducer', () => {
         },
       },
     );
+  });
 
+  test('should handle ADD_MESSAGES with object payload', () => {
     expect(
-      reducer([], {
+      reducer({
+        byId: {
+          id1: { _id: 'id1', chat: 'chatId1' },
+          id2: { _id: 'id2', chat: 'chatId1' },
+          id3: { _id: 'id3', chat: 'chatId2' },
+        },
+        allIds: ['id1', 'id2', 'id3'],
+        byChats: {
+          chatId1: ['id1', 'id2'],
+          chatId2: ['id3'],
+        },
+      }, {
+        type: actionNames.ADD_MESSAGES,
+        payload: { _id: 'id4', chat: 'chatId2' },
+      }),
+    ).toStrictEqual(
+      {
+        byId: {
+          id1: { _id: 'id1', chat: 'chatId1' },
+          id2: { _id: 'id2', chat: 'chatId1' },
+          id3: { _id: 'id3', chat: 'chatId2' },
+          id4: { _id: 'id4', chat: 'chatId2' },
+        },
+        allIds: ['id1', 'id2', 'id3', 'id4'],
+        byChats: {
+          chatId1: ['id1', 'id2'],
+          chatId2: ['id3', 'id4'],
+        },
+      },
+    );
+  });
+
+  test('should handle CLEAR_STORE', () => {
+    expect(
+      reducer({
+        byId: {
+          id1: { _id: 'id1', chat: 'chatId1' },
+          id2: { _id: 'id2', chat: 'chatId1' },
+          id3: { _id: 'id3', chat: 'chatId2' },
+        },
+        allIds: ['id1', 'id2', 'id3'],
+        byChats: {
+          chatId1: ['id1', 'id2'],
+          chatId2: ['id3'],
+        },
+      }, {
         type: actionNames.CLEAR_STORE,
       }),
     ).toStrictEqual(
