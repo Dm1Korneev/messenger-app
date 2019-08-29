3981437bd559bc2ab640cb9a8fb15316f16a3f72
Dmitriy Korneev <Dm1korneev@gmail.com>
2019-08-29 14:43:23 +0300

    add test to users selectors

--- src/__tests__/Redux/reducers/users.test.js
+++ src/__tests__/Redux/reducers/users.test.js
@@ -8,16 +8,33 @@ describe('users reducer', () => {
     );
   });
 
-  test('should handle ADD_USERS', () => {
+  test('should handle ADD_USERS with array payload', () => {
     expect(
       reducer({}, {
         type: actionNames.ADD_USERS,
-        payload: [{ _id: 'TEST' }],
+        payload: [{ _id: 'id1' }],
       }),
     ).toStrictEqual(
       {
-        byId: { TEST: { _id: 'TEST' } },
-        allIds: ['TEST'],
+        byId: { id1: { _id: 'id1' } },
+        allIds: ['id1'],
+      },
+    );
+  });
+
+  test('should handle ADD_USERS with object payload', () => {
+    expect(
+      reducer({
+        byId: { id1: { _id: 'id1' } },
+        allIds: ['id1'],
+      }, {
+        type: actionNames.ADD_USERS,
+        payload: { _id: 'id2' },
+      }),
+    ).toStrictEqual(
+      {
+        byId: { id1: { _id: 'id1' }, id2: { _id: 'id2' } },
+        allIds: ['id1', 'id2'],
       },
     );
   });
