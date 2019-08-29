9e0c87fb3b13739ee6b818ecc7d67318e6660ceb
Dmitriy Korneev <Dm1korneev@gmail.com>
2019-08-29 14:18:53 +0300

    add test to users selectors

---
+++ src/__tests__/selectors/users.test.js
@@ -0,0 +1,26 @@
+import * as sessionSelectors from 'Selectors/session';
+
+import { notCurrentUsersSelector, usersByIdSelector } from 'Selectors/users';
+
+describe('users selectors', () => {
+  const store = {
+    users: {
+      byId: { id1: { _id: 'id1' }, id2: { _id: 'id2' }, id3: { _id: 'id3' } },
+      allIds: ['id1', 'id2', 'id3'],
+    },
+  };
+
+  test('notCurrentUsersSelector should return array of not current users', () => {
+    jest.spyOn(sessionSelectors, 'currentUserIdSelector').mockReturnValue('id2');
+
+    expect(notCurrentUsersSelector(store)).toStrictEqual(
+      [{ _id: 'id1' }, { _id: 'id3' }],
+    );
+  });
+
+  test('usersByIdSelector should return users by Id', () => {
+    expect(usersByIdSelector(store)).toStrictEqual(
+      { id1: { _id: 'id1' }, id2: { _id: 'id2' }, id3: { _id: 'id3' } },
+    );
+  });
+});
