db9ea2f9b7022cd6393e3af0982c47bcfdf6605c
Dmitriy Korneev <Dm1korneev@gmail.com>
2019-09-11 16:33:35 +0300

    add tests for MessageText component

---
+++ src/__tests__/components/MessageText.test.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import MessageText from 'Components/MessageText';
+
+jest.mock('@material-ui/core/ListItem', () => global.mockComponent('ListItem'));
+jest.mock('@material-ui/core/ListItemText', () => global.mockComponent('ListItemText'));
+jest.mock('@material-ui/core/Typography', () => global.mockComponent('Typography'));
+
+const scrollIntoViewMock = jest.fn();
+window.HTMLElement.prototype.scrollIntoView = scrollIntoViewMock;
+
+const props = {
+  isCurrentUserMessage: false,
+};
+
+describe('render MessageText component', () => {
+  let wrapper;
+
+  beforeAll(() => {
+    wrapper = global.mount(
+      <MessageText {...props} />,
+    );
+  });
+
+  test('component is render', () => {
+    expect(wrapper.find('MessageText').length).toBe(1);
+  });
+
+  test('ListItem subcomponent is render', () => {
+    expect(wrapper.find({ originalcomponent: 'ListItem' }).length).toBe(1);
+  });
+});
+
+describe('snapshot-test MessageText component', () => {
+  test('Renders correct properties', () => {
+    global.mountExpect(
+      <MessageText {...props} />,
+    ).toMatchSnapshot();
+  });
+});
