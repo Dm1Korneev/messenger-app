ec6b5c2f01c461588637344a17941235750fb37e
Dmitriy Korneev <Dm1korneev@gmail.com>
2019-09-11 16:51:40 +0300

    refactoring MainContent component

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// containers
 import withStyles from '@material-ui/core/styles/withStyles';
 import Grid from '@material-ui/core/Grid';
+
 import MessagesList from 'Containers/MessagesList';
 import MessageInput from 'Containers/MessageInput';
-
-// @material-ui
 
 function MainContent(props) {
   const { classes } = props;
