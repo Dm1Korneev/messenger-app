26578c7a8aae3652554f2999a32b28293cb03369
Dmitriy Korneev <dm1korneev@gmail.com>
2020-05-10 19:40:08 +0300

    update ChatDialog tests

--- src/Components/ChatDialog/ChatDialog.test.jsx
+++ src/Components/ChatDialog/ChatDialog.test.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import { mockComponent } from 'testing/utils';
 
 import ChatDialog from './ChatDialog';
 
-jest.mock('Components/UsersAvatar', () => global.mockComponent('UsersAvatar'));
-jest.mock('Components/AvatarSelector', () => global.mockComponent('AvatarSelector'));
+jest.mock('Components/UsersAvatar', () => mockComponent('UsersAvatar'));
+jest.mock('Components/AvatarSelector', () => mockComponent('AvatarSelector'));
 
-jest.mock('@material-ui/core/Button', () => global.mockComponent('Button'));
-jest.mock('@material-ui/core/Dialog', () => global.mockComponent('Dialog'));
-jest.mock('@material-ui/core/DialogActions', () => global.mockComponent('DialogActions'));
-jest.mock('@material-ui/core/DialogContent', () => global.mockComponent('DialogContent'));
-jest.mock('@material-ui/core/DialogTitle', () => global.mockComponent('DialogTitle'));
-jest.mock('@material-ui/core/ListItem', () => global.mockComponent('ListItem'));
-jest.mock('@material-ui/core/List', () => global.mockComponent('List'));
-jest.mock('@material-ui/core/ListItemText', () => global.mockComponent('ListItemText'));
+jest.mock('@material-ui/core/Button', () => mockComponent('Button'));
+jest.mock('@material-ui/core/Dialog', () => mockComponent('Dialog'));
+jest.mock('@material-ui/core/DialogActions', () => mockComponent('DialogActions'));
+jest.mock('@material-ui/core/DialogContent', () => mockComponent('DialogContent'));
+jest.mock('@material-ui/core/DialogTitle', () => mockComponent('DialogTitle'));
+jest.mock('@material-ui/core/ListItem', () => mockComponent('ListItem'));
+jest.mock('@material-ui/core/List', () => mockComponent('List'));
+jest.mock('@material-ui/core/ListItemText', () => mockComponent('ListItemText'));
 
 const props = {
   closeChatDialog: jest.fn(),
@@ -36,24 +38,14 @@ const props = {
 };
 
 describe('render ChatDialog component', () => {
-  let wrapper;
+  const setup = () => render(
+    <ChatDialog {...props} />,
+  );
 
-  beforeAll(() => {
-    wrapper = global.mount(
-      <ChatDialog {...props} />,
-    );
-  });
+  test('snapshot test', async () => {
+    const { asFragment } = setup();
 
-  test('component is render', () => {
-    expect(wrapper.find('ChatDialog').length).toBe(1);
-  });
-
-  test('Dialog subcomponent is render', () => {
-    expect(wrapper.find({ originalcomponent: 'Dialog' }).length).toBe(1);
-  });
-
-  test('DialogContent subcomponent is render', () => {
-    expect(wrapper.find({ originalcomponent: 'DialogContent' }).length).toBe(1);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
 
