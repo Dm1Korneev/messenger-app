cefe2d3ceb4f76ea809b95952acaf2ada81787c2
Dmitriy Korneev <dm1korneev@gmail.com>
2020-05-10 20:38:45 +0300

    update tests UsersAvatar component

--- src/Components/UsersAvatar/UsersAvatar.test.jsx
+++ src/Components/UsersAvatar/UsersAvatar.test.jsx
@@ -1,41 +1,26 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import { mockComponent } from 'testing/utils';
 
 import UsersAvatar from './UsersAvatar';
 
-jest.mock('@material-ui/core/ListItemAvatar', () => global.mockComponent('ListItemAvatar'));
-jest.mock('@material-ui/core/Avatar', () => global.mockComponent('Avatar'));
-jest.mock('@material-ui/icons/AccountCircleOutlined', () => global.mockComponent('AccountIcon'));
+jest.mock('@material-ui/core/ListItemAvatar', () => mockComponent('ListItemAvatar'));
+jest.mock('@material-ui/core/Avatar', () => mockComponent('Avatar'));
+jest.mock('@material-ui/icons/AccountCircleOutlined', () => mockComponent('AccountIcon'));
 
 describe('render UsersAvatar component', () => {
-  let wrapper;
-
   const props = {
     size: 60,
   };
 
-  beforeAll(() => {
-    wrapper = global.mount(
-      <UsersAvatar {...props} />,
-    );
-  });
+  const setup = () => render(
+    <UsersAvatar {...props} />,
+  );
 
-  test('component is render', () => {
-    expect(wrapper.find('UsersAvatar').length).toBe(1);
-  });
+  test('snapshot test', async () => {
+    const { asFragment } = setup();
 
-  test('ListItemAvatar subcomponent is render', () => {
-    wrapper.update();
-    expect(wrapper.find({ originalcomponent: 'ListItemAvatar' }).length).toBe(1);
-  });
-
-  test('Avatar subcomponent is render', () => {
-    wrapper.update();
-    expect(wrapper.find({ originalcomponent: 'Avatar' }).length).toBe(1);
-  });
-
-  test('AccountIcon subcomponent is render', () => {
-    wrapper.update();
-    expect(wrapper.find({ originalcomponent: 'AccountIcon' }).length).toBe(1);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
 
