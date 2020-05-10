6167202d515de81a019fcd3f289a098e7db25af5
Dmitriy Korneev <dm1korneev@gmail.com>
2020-05-10 20:13:35 +0300

    update UserEmailField tests

--- src/Components/UserEmailField/UserEmailField.test.jsx
+++ src/Components/UserEmailField/UserEmailField.test.jsx
@@ -1,32 +1,27 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import { mockComponent } from 'testing/utils';
 
 import UserEmailField from './UserEmailField';
 
 jest.mock('react-material-ui-form-validator', () => ({
-  TextValidator: global.mockComponent('TextValidator'),
+  TextValidator: mockComponent('TextValidator'),
 }));
 
 describe('render UserEmailField component', () => {
-  let wrapper;
-
   const props = {
     value: 'TEST',
     onChange: jest.fn(),
   };
 
-  beforeAll(() => {
-    wrapper = global.mount(
-      <UserEmailField {...props} />,
-    );
-  });
+  const setup = () => render(
+    <UserEmailField {...props} />,
+  );
 
-  test('component is render', () => {
-    expect(wrapper.find('UserEmailField').length).toBe(1);
-  });
+  test('snapshot test', async () => {
+    const { asFragment } = setup();
 
-  test('TextValidator subcomponent is render', () => {
-    wrapper.update();
-    expect(wrapper.find({ originalcomponent: 'TextValidator' }).length).toBe(1);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
 
