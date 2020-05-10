0d96cee34fa95d4dd26142e71fff328bfb5f72d0
Dmitriy Korneev <dm1korneev@gmail.com>
2020-05-10 20:13:12 +0300

    update SignIn tests

--- src/Components/SignIn/SignIn.test.jsx
+++ src/Components/SignIn/SignIn.test.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import { mockComponent } from 'testing/utils';
 
 import SignIn from './SignIn';
 
-jest.mock('Components/UserEmailField', () => global.mockComponent('UserEmailField'));
-jest.mock('Components/UserNameField', () => global.mockComponent('UserNameField'));
-jest.mock('Components/UserPasswordField', () => global.mockComponent('UserPasswordField'));
-jest.mock('Components/AvatarSelector', () => global.mockComponent('AvatarSelector'));
+jest.mock('Components/UserEmailField', () => mockComponent('UserEmailField'));
+jest.mock('Components/UserNameField', () => mockComponent('UserNameField'));
+jest.mock('Components/UserPasswordField', () => mockComponent('UserPasswordField'));
+jest.mock('Components/AvatarSelector', () => mockComponent('AvatarSelector'));
 
-jest.mock('@material-ui/core/Avatar', () => global.mockComponent('Avatar'));
-jest.mock('@material-ui/core/Button', () => global.mockComponent('Button'));
-jest.mock('@material-ui/core/CssBaseline', () => global.mockComponent('CssBaseline'));
-jest.mock('@material-ui/core/FormControlLabel', () => global.mockComponent('FormControlLabel'));
-jest.mock('@material-ui/core/Checkbox', () => global.mockComponent('Checkbox'));
-jest.mock('@material-ui/core/Paper', () => global.mockComponent('Paper'));
-jest.mock('@material-ui/core/Typography', () => global.mockComponent('Typography'));
-jest.mock('@material-ui/core/AppBar', () => global.mockComponent('AppBar'));
-jest.mock('@material-ui/core/Tabs', () => global.mockComponent('Tabs'));
-jest.mock('@material-ui/core/Tab', () => global.mockComponent('Tab'));
-jest.mock('@material-ui/core/FormHelperText', () => global.mockComponent('FormHelperText'));
-jest.mock('@material-ui/icons/LockOutlined', () => global.mockComponent('LockOutlined'));
-jest.mock('@material-ui/icons/PersonAddOutlined', () => global.mockComponent('PersonAddOutlined'));
+jest.mock('@material-ui/core/Avatar', () => mockComponent('Avatar'));
+jest.mock('@material-ui/core/Button', () => mockComponent('Button'));
+jest.mock('@material-ui/core/CssBaseline', () => mockComponent('CssBaseline'));
+jest.mock('@material-ui/core/FormControlLabel', () => mockComponent('FormControlLabel'));
+jest.mock('@material-ui/core/Checkbox', () => mockComponent('Checkbox'));
+jest.mock('@material-ui/core/Paper', () => mockComponent('Paper'));
+jest.mock('@material-ui/core/Typography', () => mockComponent('Typography'));
+jest.mock('@material-ui/core/AppBar', () => mockComponent('AppBar'));
+jest.mock('@material-ui/core/Tabs', () => mockComponent('Tabs'));
+jest.mock('@material-ui/core/Tab', () => mockComponent('Tab'));
+jest.mock('@material-ui/core/FormHelperText', () => mockComponent('FormHelperText'));
+jest.mock('@material-ui/icons/LockOutlined', () => mockComponent('LockOutlined'));
+jest.mock('@material-ui/icons/PersonAddOutlined', () => mockComponent('PersonAddOutlined'));
 
 const props = {
   onSignIn: jest.fn(),
@@ -27,25 +29,13 @@ const props = {
 };
 
 describe('render SignIn component', () => {
-  let wrapper;
+  const setup = () => render(
+    <SignIn {...props} />,
+  );
 
-  beforeAll(() => {
-    wrapper = global.mount(
-      <SignIn {...props} />,
-    );
-  });
+  test('snapshot test', async () => {
+    const { asFragment } = setup();
 
-  test('component is render', () => {
-    expect(wrapper.find('SignIn').length).toBe(1);
-  });
-
-  test('UserEmailField subcomponent is render', () => {
-    wrapper.update();
-    expect(wrapper.find({ originalcomponent: 'UserEmailField' }).length).toBe(1);
-  });
-
-  test('UserPasswordField subcomponent is render', () => {
-    wrapper.update();
-    expect(wrapper.find({ originalcomponent: 'UserPasswordField' }).length).toBe(1);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
