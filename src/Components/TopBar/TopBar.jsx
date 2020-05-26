2dcd9eda32f34916a34d928ff102d9dfda31bef9
Dmitriy Korneev <dm1korneev@gmail.com>
2020-05-26 23:47:53 +0300

    update styles

--- src/Components/TopBar/TopBar.jsx
+++ src/Components/TopBar/TopBar.jsx
@@ -1,84 +1,21 @@
 import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 import MenuIcon from '@material-ui/icons/Menu';
 import CreateIcon from '@material-ui/icons/Create';
 
 import { DRAWER_WIDTH } from 'Constants';
 
-function TopBar(props) {
-  const {
-    drawerIsOpen,
-    user,
-    onLogout,
-    onDriwerOpen,
-    openModifyUserDialog,
-    classes,
-  } = props;
-  const userName = user && user.name;
-
-  return (
-    <AppBar
-      position="absolute"
-      className={classNames(
-        classes.appBar,
-        drawerIsOpen && classes.appBarShift,
-      )}
-    >
-      <Toolbar disableGutters={!drawerIsOpen} className={classes.toolbar}>
-        <IconButton
-          color="inherit"
-          aria-label="Open drawer"
-          onClick={onDriwerOpen}
-          className={classNames(
-            classes.menuButton,
-            drawerIsOpen && classes.menuButtonHidden,
-          )}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography
-          component="h1"
-          variant="h6"
-          color="inherit"
-          noWrap
-          className={classes.title}
-        >
-          {userName}
-          <IconButton color="inherit" onClick={openModifyUserDialog}>
-            <CreateIcon />
-          </IconButton>
-        </Typography>
-        <Button color="inherit" onClick={onLogout}>
-          logout
-        </Button>
-      </Toolbar>
-    </AppBar>
-  );
-}
-
-TopBar.propTypes = {
-  classes: PropTypes.instanceOf(Object).isRequired,
-  drawerIsOpen: PropTypes.bool.isRequired,
-  onLogout: PropTypes.func.isRequired,
-  onDriwerOpen: PropTypes.func.isRequired,
-  openModifyUserDialog: PropTypes.func.isRequired,
-  user: PropTypes.shape({
-    name: PropTypes.string,
-  }).isRequired,
-};
-
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
-    boxShadow: 'none',
-    borderBottom: '1px solid '.concat(theme.palette.divider),
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
@@ -92,20 +29,67 @@ const styles = (theme) => ({
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
-  toolbar: {
-    paddingRight: theme.spacing(3), // keep right padding when drawer closed
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-});
+}));
 
-export default withStyles(styles)(TopBar);
+function TopBar(props) {
+  const classes = useStyles();
+
+  const {
+    drawerIsOpen,
+    user,
+    onLogout,
+    onDriwerOpen,
+    openModifyUserDialog,
+  } = props;
+  const userName = user && user.name;
+
+  return (
+    <AppBar
+      position="fixed"
+      className={classNames(
+        classes.appBar,
+        drawerIsOpen && classes.appBarShift,
+      )}
+    >
+      <Toolbar>
+        <Box mr={2} hidden={drawerIsOpen}>
+          <IconButton
+            color="inherit"
+            edge="start"
+            aria-label="Open drawer"
+            onClick={onDriwerOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Box>
+        <Box flexGrow="1">
+          <Typography
+            variant="h6"
+            color="inherit"
+          >
+            {userName}
+            <IconButton color="inherit" onClick={openModifyUserDialog}>
+              <CreateIcon />
+            </IconButton>
+          </Typography>
+        </Box>
+        <Button color="inherit" onClick={onLogout}>
+          logout
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+TopBar.propTypes = {
+  drawerIsOpen: PropTypes.bool.isRequired,
+  onLogout: PropTypes.func.isRequired,
+  onDriwerOpen: PropTypes.func.isRequired,
+  openModifyUserDialog: PropTypes.func.isRequired,
+  user: PropTypes.shape({
+    name: PropTypes.string,
+  }).isRequired,
+};
+
+export default TopBar;
 
