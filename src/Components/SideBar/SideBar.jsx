1d5d26ff93883773f1de1fa34d24ba58f41ade84
Dmitriy Korneev <dm1korneev@gmail.com>
2020-05-29 23:10:26 +0300

    update SideBar

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,129 +1,39 @@
-import React from 'react';
-import classNames from 'classnames';
+import React, { useEffect } from 'react';
+import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
+import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import Drawer from '@material-ui/core/Drawer';
 import ListItem from '@material-ui/core/ListItem';
 import List from '@material-ui/core/List';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import AddBoxIcon from '@material-ui/icons/AddBox';
+import AddBox from '@material-ui/icons/AddBox';
 
 import { DRAWER_WIDTH, RELOAD_PERIOD } from 'Constants';
 import Chat from 'Components/Chat';
 
-class SideBar extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.getChats = props.getChats;
-    this.interval = undefined;
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(this.getChats, RELOAD_PERIOD);
-  }
-
-  componentWillUnmount() {
-    if (this.interval) {
-      clearInterval(this.interval);
-    }
-  }
-
-  render() {
-    const {
-      drawerIsOpen,
-      chats,
-      activeChat,
-      onDrawerClose,
-      classes,
-      changeActiveChat,
-      openAddChatDialog,
-      openModifyChatDialog,
-    } = this.props;
-
-    return (
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: classNames(
-            classes.drawerPaper,
-            !drawerIsOpen && classes.drawerPaperClose,
-          ),
-        }}
-        open={drawerIsOpen}
-      >
-        <div className={classes.toolbarIcon}>
-          <IconButton onClick={onDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          <ListItem button key="add_chat" onClick={openAddChatDialog}>
-            <ListItemAvatar>
-              <AddBoxIcon color="primary" className={classes.addChatIcon} />
-            </ListItemAvatar>
-            <ListItemText primary="Add chat" />
-          </ListItem>
-
-          {chats.map((chat) => {
-            const { _id } = chat;
-            return (
-              <Chat
-                chat={chat}
-                key={_id}
-                selected={_id === activeChat}
-                chatOnClick={() => changeActiveChat({ activeChat: _id })}
-                chatModifyOnClick={openModifyChatDialog}
-              />
-            );
-          })}
-        </List>
-      </Drawer>
-    );
-  }
-}
-
-SideBar.defaultProps = {
-  activeChat: null,
-  chats: [],
-};
-SideBar.propTypes = {
-  classes: PropTypes.instanceOf(Object).isRequired,
-  getChats: PropTypes.func.isRequired,
-  drawerIsOpen: PropTypes.bool.isRequired,
-  chats: PropTypes.arrayOf(PropTypes.instanceOf(Object)),
-  activeChat: PropTypes.string,
-  onDrawerClose: PropTypes.func.isRequired,
-  changeActiveChat: PropTypes.func.isRequired,
-  openAddChatDialog: PropTypes.func.isRequired,
-  openModifyChatDialog: PropTypes.func.isRequired,
-};
-
-const styles = (theme) => ({
-  drawerPaper: {
-    position: 'relative',
+const useStyles = makeStyles((theme) => ({
+  drawer: {
+    overflowX: 'hidden',
     whiteSpace: 'nowrap',
+    position: 'relative',
+  },
+  drawerOpen: {
     width: DRAWER_WIDTH,
     transition: theme.transitions.create('width', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
-  drawerPaperClose: {
-    overflowX: 'hidden',
+  drawerClose: {
+    width: theme.spacing(9),
     transition: theme.transitions.create('width', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
   },
   toolbarIcon: {
     display: 'flex',
@@ -136,7 +46,84 @@ const styles = (theme) => ({
     fontSize: theme.spacing(6),
     marginLeft: -theme.spacing(0.5),
   },
-});
+}));
 
-export default withStyles(styles)(SideBar);
+const SideBar = ({
+  getChats, drawerIsOpen,
+  chats,
+  activeChat,
+  onDrawerClose,
+  changeActiveChat,
+  openAddChatDialog,
+  openModifyChatDialog,
+}) => {
+  useEffect(() => {
+    const interval = setInterval(getChats, RELOAD_PERIOD);
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [getChats]);
+  const classes = useStyles();
+
+  return (
+    <Drawer
+      variant="permanent"
+      classes={{
+        paper: clsx(
+          classes.drawer, {
+            [classes.drawerOpen]: drawerIsOpen,
+            [classes.drawerClose]: !drawerIsOpen,
+          },
+        ),
+      }}
+      open={drawerIsOpen}
+    >
+      <div className={classes.toolbarIcon}>
+        <IconButton onClick={onDrawerClose}>
+          <ChevronLeftIcon />
+        </IconButton>
+      </div>
+      <Divider />
+      <List>
+        <ListItem button key="add_chat" onClick={openAddChatDialog}>
+          <ListItemIcon>
+            <AddBox color="primary" className={classes.addChatIcon} />
+          </ListItemIcon>
+          <ListItemText primary="Add chat" />
+        </ListItem>
+        {chats.map((chat) => {
+          const { _id } = chat;
+          return (
+            <Chat
+              chat={chat}
+              key={_id}
+              selected={_id === activeChat}
+              chatOnClick={() => changeActiveChat({ activeChat: _id })}
+              chatModifyOnClick={openModifyChatDialog}
+            />
+          );
+        })}
+      </List>
+    </Drawer>
+  );
+};
+
+SideBar.defaultProps = {
+  activeChat: null,
+  chats: [],
+};
+SideBar.propTypes = {
+  getChats: PropTypes.func.isRequired,
+  drawerIsOpen: PropTypes.bool.isRequired,
+  chats: PropTypes.arrayOf(PropTypes.instanceOf(Object)),
+  activeChat: PropTypes.string,
+  onDrawerClose: PropTypes.func.isRequired,
+  changeActiveChat: PropTypes.func.isRequired,
+  openAddChatDialog: PropTypes.func.isRequired,
+  openModifyChatDialog: PropTypes.func.isRequired,
+};
+
+export default SideBar;
 
