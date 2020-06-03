372896b8b933a7ab8a4aad3cec26e1cdd2ff0eb4
Dmitriy Korneev <dm1korneev@gmail.com>
2020-06-03 21:41:13 +0300

    update MessageInput component

--- src/Components/MessageText/MessageText.jsx
+++ src/Components/MessageText/MessageText.jsx
@@ -1,66 +1,27 @@
 import React from 'react';
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-function MessageText(props) {
-  const { text, isCurrentUserMessage, classes } = props;
-
-  const ListItemClass = classNames(
-    classes.ListItem,
-    isCurrentUserMessage && classes.ListItem_reverse,
-  );
+const MessageText = ({ text, isCurrentUserMessage }) => {
+  const flexDirection = isCurrentUserMessage ? 'row' : 'row-reverse';
 
   return (
-    <ListItem className={ListItemClass}>
-      <div className={classes.textContent}>
-        <ListItemText
-          className={classes.ListItemText}
-          primary={(
-            <Typography component="span" className={classes.ListItemText__text}>
-              {text}
-            </Typography>
-)}
-        />
-      </div>
-    </ListItem>
+    <Box display="flex" flex="1" flexDirection={flexDirection} pt={1} px={2}>
+      <Typography variant="body2">
+        {text}
+      </Typography>
+    </Box>
   );
-}
+};
 
 MessageText.defaultProps = {
   text: '',
 };
 MessageText.propTypes = {
-  classes: PropTypes.instanceOf(Object).isRequired,
   text: PropTypes.string,
   isCurrentUserMessage: PropTypes.bool.isRequired,
 };
 
-const styles = (theme) => ({
-  ListItemText: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    '&:first-child': {
-      paddingLeft: theme.spacing(2),
-    },
-  },
-  ListItem_reverse: {
-    flexDirection: 'row-reverse',
-  },
-  ListItem: {
-    alignItems: 'start',
-    padding: theme.spacing(1, 0, 0, 0),
-  },
-  textContent: {
-    flexGrow: 1,
-  },
-  ListItemText__text: {
-    fontSize: theme.typography.pxToRem(13),
-  },
-});
-
-export default withStyles(styles)(MessageText);
+export default MessageText;
 
