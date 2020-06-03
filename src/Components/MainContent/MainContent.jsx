a74739137b8818bf115358661c5a9f6bc7aafe76
Dmitriy Korneev <dm1korneev@gmail.com>
2020-06-03 23:02:00 +0300

    update MainContent component

--- src/Components/MainContent/MainContent.jsx
+++ src/Components/MainContent/MainContent.jsx
@@ -1,46 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 
 import MessageInput from 'Components/MessageInput';
 import MessagesList from 'Components/MessagesList';
 
-function MainContent(props) {
-  const { classes } = props;
-
-  return (
-    <main className={classes.main}>
-      <Grid item xs={12} sm={11} md={10} lg={9} className={classes.grid}>
-        <div className={classes.content}>
-          <div className={classes.appBarSpacer} />
-          <MessagesList />
-          <MessageInput />
-        </div>
-      </Grid>
-    </main>
-  );
-}
-
-MainContent.propTypes = {
-  classes: PropTypes.instanceOf(Object).isRequired,
-};
-
-const styles = (theme) => ({
-  main: {
-    flexGrow: 1,
-  },
+const useStyles = makeStyles((theme) => ({
   grid: {
     margin: '0 auto',
     height: '100%',
   },
   appBarSpacer: { marginTop: theme.spacing(1), ...theme.mixins.toolbar },
-  content: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-  },
-});
+}));
 
-export default withStyles(styles)(MainContent);
+function MainContent() {
+  const classes = useStyles();
+
+  return (
+    <Box>
+      <Grid item xs={12} sm={11} md={10} lg={9} className={classes.grid}>
+        <Box display="flex" flexDirection="column" justifyContent="space-between" height="100%">
+          <Box className={classes.appBarSpacer} />
+          <MessagesList />
+          <MessageInput />
+        </Box>
+      </Grid>
+    </Box>
+  );
+}
+
+export default MainContent;
