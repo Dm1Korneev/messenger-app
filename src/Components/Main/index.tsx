299631efbff394c08f748604bacd39d109aa8961
Dm1Korneev <dm1korneev@gmail.com>
2022-07-22 14:18:06 +0100

    Add test

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -1,17 +1,1 @@
-
-import { App } from 'Components/App';
-import { QueryClientProvider } from 'Components/QueryClientProvider';
-import { SessionProvider } from 'Components/SessionProvider';
-import { ThemeProvider } from 'Components/ThemeProvider';
-
-const Main = () => (
-  <ThemeProvider>
-    <QueryClientProvider>
-      <SessionProvider>
-        <App />
-      </SessionProvider>
-    </QueryClientProvider>
-  </ThemeProvider>
-);
-
-export default Main;
+export * from './Main';
