521de6e16c9b613307e14cce57e6f8a40936d810
Dm1Korneev <dm1korneev@gmail.com>
2022-07-26 14:52:35 +0100

    update port binding

--- src/Server/main.ts
+++ src/Server/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 
 bootstrap();
