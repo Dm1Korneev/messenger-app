f414ca8ef7d033eb837f81d3942078c4c6e6d356
Dm1Korneev <dm1korneev@gmail.com>
2023-06-04 00:16:41 +0100

    improve typings

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -2,5 +2,5 @@ export * from './chat';
 export * from './message';
 export * from './user';
 export * from './auth';
-export * from './db-document';
+export * from './db-document.d';
 export * from './base-types';
